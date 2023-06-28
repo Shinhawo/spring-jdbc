840f04a2aeb2c800b2eb227053c0fc8111155b20
신하원 <hownu080@naver.com>
2023-06-28 14:56:16 +0900

    spring jdbc (jdbcTemplate, connection pool) 실습

---
+++ src/main/java/dao/DeptDao.java
@@ -0,0 +1,54 @@
+package dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import vo.Dept;
+
+@Repository
+public class DeptDao {
+
+	@Autowired
+	private JdbcTemplate template;
+	
+	public List<Dept> getAllDepts(){
+		String sql = "select department_id, department_name, location_id, manager_id "
+				   + "from departments order by department_id asc ";
+		return template.query(sql, (rs, rownum)->{
+			Dept dept = new Dept();
+			dept.setId(rs.getInt("department_id"));
+			dept.setName(rs.getString("department_name"));
+			dept.setManagerId(rs.getInt("manager_id"));
+			dept.setLocationId(rs.getInt("location_id"));
+			
+			return dept;	
+		});
+	}
+	
+	public void insertDept(Dept dept) {
+		String sql = "insert into departments "
+				   + "(department_id, department_name, location_id) "
+				   + "values "
+				   + "(departments_seq.nextval, ?, ?)";
+		
+		template.update(sql, dept.getName(), dept.getLocationId());
+	}
+	
+	public Dept getDeptById(int departmentId) {
+		String sql = "select department_id, department_name, location_id, manager_id "
+				   + "from departments where department_id = ? ";
+		
+		return template.queryForObject(sql, (rs, rowNum) -> {
+			Dept dept = new Dept();
+			dept.setId(rs.getInt("department_id"));
+			dept.setName(rs.getString("department_name"));
+			dept.setManagerId(rs.getInt("manager_id"));
+			dept.setLocationId(rs.getInt("location_id"));
+			
+			return dept;
+		}, departmentId );
+	}
+}
